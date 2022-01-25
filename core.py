3191b3745f970d8ef2bc7c73d6bc77f2d1b2eadf
Nam4802 <58206992+Nam4802@users.noreply.github.com>
2022-01-25 12:05:30 -0500

    Forgot to import from midigen to core

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ import random
 import math
 from midiutil import MIDIFile
 from generator import genchord, genriff
+from midigen import addChordMidi, addRiffMidi
 
 sname = {'majscale':'Major scale', 'minscale':'Minor scale'}
 chartopc = {'C':0, 'C#':1, 'Db':1, 'D':2, 'D#':3, 'Eb':3, 'E':4, 'F':5, 'F#':6, 'Gb':6, 'G':7, 'G#':8, 'Ab':8, 'A':9, 'A#':10, 'Bb':10, 'B':11}
